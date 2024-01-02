92846fb30f8715ad7d495d38b58c2cd93642365f
sonia <https://github.com/Soniabensaad>
2024-01-02 16:58:47 +0100

    typescript

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -43,3 +43,45 @@ export interface Teacher {
   const printedTeacher = printTeacher("kamelia", "Jeo");
   console.log(printedTeacher); 
   
+  export interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+  }
+  
+  // Interface for the StudentClass
+  export interface Student {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  // Class definition
+  export class StudentClass implements Student {
+    // Properties
+    firstName: string;
+    lastName: string;
+  
+    // Constructor
+    constructor({ firstName, lastName }: StudentConstructor) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    // Methods
+    workOnHomework(): string {
+      return "Currently working";
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  }
+  
+  // Example usage
+  const studentData: StudentConstructor = {
+    firstName: "John",
+    lastName: "Doe",
+  };
+  
+  const studentInstance = new StudentClass(studentData);
+  console.log(studentInstance.displayName()); // Output: John
+  console.log(studentInstance.workOnHomework());
