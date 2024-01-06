811cd246278da920ed92858e2221d1ced1dc56ea
sonia <https://github.com/Soniabensaad>
2024-01-06 23:53:28 +0100

    typescript

---
+++ TypeScript/task_5/js/main.ts
@@ -0,0 +1,27 @@
+interface MajorCredits {
+    credits: number;
+    brand: 'MajorCredits';
+  }
+  
+  // Define MinorCredits interface
+  interface MinorCredits {
+    credits: number;
+    brand: 'MinorCredits';
+  }
+  
+  // Function to sum major credits
+  function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return {
+      credits: subject1.credits + subject2.credits,
+      brand: 'MajorCredits',
+    };
+  }
+  
+  // Function to sum minor credits
+  function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return {
+      credits: subject1.credits + subject2.credits,
+      brand: 'MinorCredits',
+    };
+  }
+  
