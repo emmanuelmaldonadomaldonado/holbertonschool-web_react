4f495f640fe5103f53d7ca2c9ac804f98e9fe477
sonia <https://github.com/Soniabensaad>
2024-01-07 17:25:52 +0100

    webpack

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,20 +1,25 @@
+import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
-$(function() {
-let counter = 0;
+let count = 0;
+
 function updateCounter() {
-    counter++;
-    $('#count').html(`${counter} clicks on the button`);
-};
+	count += 1;
+	return count;
+}
 
-
+$(function() {
+	$('body').append("<div id='logo'></div>");
 	$('body').append('<p>Holberton Dashboard</p>');
 	$('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button>Click here to get started</button>');
-    $('body').append("<p id='count'></p>");
+	$('body').append('<button><span>Click here to get started<span></button>');
+	$('body').append("<p id='count'></p>");
 	$('body').append('<p>Copyright - Holberton School</p>');
 
-
-$('button').on('click', _.debounce(updateCounter, 500));
+	let debouncedFunc = _.debounce(() => {
+		let count = updateCounter();
+		$('#count').text(`${count} clicks on the button`);
+	});
+	$('button').on('click', debouncedFunc);
 });
