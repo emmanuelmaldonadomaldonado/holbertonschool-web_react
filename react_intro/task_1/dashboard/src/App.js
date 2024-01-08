9fc175c4156dbb327f1150471a38682ae5685cd5
sonia <https://github.com/Soniabensaad>
2024-01-08 16:36:35 +0100

    react

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,29 @@
+// App.js
+
+import logo from './holberton_logo.jpg';
+import './App.css';
+import React from 'react';
+import { getFullYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <Notifications />
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,11 @@
+// utils.js
+
+export function getFullYear() {
+    const currentYear = new Date().getFullYear();
+    return currentYear;
+  }
+  
+  export function getFooterCopy(isIndex) {
+    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+  }
+  
