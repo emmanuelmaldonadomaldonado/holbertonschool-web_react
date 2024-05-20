1f855bba316fc0948f8933f3693464bbf35712aa
sonia <https://github.com/Soniabensaad>
2024-05-20 15:41:48 +0200

    unitest

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,25 +1,25 @@
-const $ = require("jquery");
-const _ = require("lodash");
-import "../css/main.css";
+import '../css/main.css';
+import $ from 'jquery';
+import _ from 'lodash';
 
 let count = 0;
 
 function updateCounter() {
-  count++;
-  $("#count").text(`${count} clicks on the button`);
+	count += 1;
+	return count;
 }
 
-const $button = $("<button>Click here to get started</button>").on(
-  "click",
-  _.debounce(updateCounter, 500, {
-    leading: true,
-    trailing: false,
-  })
-);
+$(function() {
+	$('body').append("<div id='logo'></div>");
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<button><span>Click here to get started<span></button>');
+	$('body').append("<p id='count'></p>");
+	$('body').append('<p>Copyright - Holberton School</p>');
 
-$("body").append("<div id='logo'></div>");
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append($button);
-$("body").append("<p id='count'></p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+	let debouncedFunc = _.debounce(() => {
+		let count = updateCounter();
+		$('#count').text(`${count} clicks on the button`);
+	});
+	$('button').on('click', debouncedFunc);
+});
--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,34 +1,16 @@
-onst path = require("path");
+button {
+	text-align: right;
+}
 
-module.exports = {
-  mode: "production",
-  entry: "./js/dashboard_main.js",
-  performance: {
-    maxAssetSize: 1000000,
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-    ],
-  },
-};
+button > span {
+	font-style:normal;
+	font-weight: bold;
+}
+
+#logo {
+	width: 200px;
+	height: 200px;
+	background: url('../assets/holberton-logo.jpg');
+	background-position: center;
+	background-size: contain;
+}
